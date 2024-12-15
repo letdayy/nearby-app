8ad99fa7a21a96f741da9c298defa785a7a7a6d7
letdayy <lcdayane49@gmail.com>
2024-12-15 00:19:52 -0300

    chore: add project information

---
+++ NearbyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyApp
+//
+//  Created by leticia.dayane on 14/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
